f22ae3fa74141d1f88f657aa36a997cdce672dd4
Ope Adelasoye <on861917@dal.ca>
2023-07-27 21:19:25 -0300

    Added sl4fj dependency so tests compile. Adjusted TestGC so it doesn't run for hours

--- src/test/java/com/alibaba/json/TestGC.java
+++ src/test/java/com/alibaba/json/TestGC.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 
 public class TestGC extends TestCase {
     public void test_0 () throws Exception {
-        for (int i = 0; i < 1000 * 1000; ++i) {
+        for (int i = 0; i < 1000; ++i) {
             StringBuilder buf = new StringBuilder(1000 * 1000 * 10);
             buf.append(i);
             Thread.sleep(10);
